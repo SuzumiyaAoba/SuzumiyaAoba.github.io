d5075eb609233c6606bf03378a5a8e43b7874021
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2024-08-05 23:59:44 +0900

    chore: config for SSG

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,13 @@ const isProd = process.env.NODE_ENV === "production";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // SSG
   output: "export",
   assetPrefix: isProd ? "https://suzumiyaaoba.github.io" : undefined,
+  images: {
+    unoptimized: !isProd,
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
