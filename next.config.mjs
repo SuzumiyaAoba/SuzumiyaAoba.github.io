ff3eaea421d74c852d4c08aa014a026544697c84
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2024-08-05 23:45:18 +0900

    chore: set assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
+  assetPrefix: isProd ? "https://suzumiyaaoba.github.io" : undefined,
 };
 
 export default nextConfig;
