78b6819b2ae0a1f17916d7def33dfba70065acd1
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2024-12-17 09:01:05 +0900

    feat: add contact form

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,16 @@
+export default async function Contact() {
+  return (
+    <main className="flex flex-col w-full max-w-4xl mx-auto mt-8 px-4 pb-16">
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSeXOZA4Mriinisf4yXq8Y9XxfiSNvWjF_qhg5qFYY8af85bfQ/viewform?embedded=true"
+        width="640"
+        height="821"
+        frameBorder="0"
+        marginHeight={0}
+        marginWidth={0}
+      >
+        読み込んでいます…
+      </iframe>
+    </main>
+  );
+}
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -14,9 +14,12 @@ export const Footer: FC<{
 
   return (
     <footer className="flex flex-col font-light items-center mt-auto pt-8 pb-8 bg-gray-100">
-      <div className="flex mb-4">
+      <div className="flex gap-x-6 mb-4">
         <a className="hover:underline" href="/privacy-policy/">
           プライバシーポリシー
+        </a>
+        <a className="hover:underline" href="/contact">
+          お問い合わせ
         </a>
       </div>
       <div className="text-sm">
