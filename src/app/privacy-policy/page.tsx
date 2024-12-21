3217454d080b3134c039d469db15f6d6d2de2038
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2024-12-21 17:08:59 +0900

    feat: update privacy policy

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -2,7 +2,18 @@ export default async function PrivacyPolicy() {
   return (
     <main className="flex flex-col w-full max-w-4xl mx-auto px-4 pb-16">
       <h1 className="my-8 text-3xl">プライバシーポリシー</h1>
-      <div>
+      <h2 className="mb-4 text-xl font-bold">個人情報の利用目的</h2>
+      <div className="mb-8">
+        当サイトでは、訪問者から取得した個人情報（名前、メールアドレスなど）を以下の目的で利用します。
+        <ul>
+          <li>お問い合わせへの対応</li>
+          <li>サービス向上のための分析</li>
+        </ul>
+        取得した個人情報は、適切な方法で管理し、法令に基づいて安全に保護されます。
+      </div>
+
+      <h2 className="mb-4 text-xl font-bold">Google Analytics</h2>
+      <div className="mb-8">
         当サイトでは、Googleによるアクセス解析ツール「Googleアナリティクス」を使用しています。このGoogleアナリティクスはデータの収集のためにCookieを使用しています。このデータは匿名で収集されており、個人を特定するものではありません。
         この機能はCookieを無効にすることで収集を拒否することが出来ますので、お使いのブラウザの設定をご確認ください。この規約に関しての詳細は
         <a
@@ -21,6 +32,11 @@ export default async function PrivacyPolicy() {
           Googleポリシーと規約ページ
         </a>
         をご覧ください。
+      </div>
+
+      <h2 className="mb-4 text-xl font-bold">免責事項</h2>
+      <div className="mb-8">
+        当サイトに掲載する情報は、正確性を期していますが、その内容の信頼性や安全性について保証するものではありません。訪問者が当サイトの情報を利用して行う一切の行動について、当サイト運営者は責任を負いかねます。自己責任のもとでご利用ください。
       </div>
     </main>
   );
