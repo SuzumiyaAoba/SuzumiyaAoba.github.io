16e53f29fa015c8a684e2a7fb4181e638e4fc58f
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2025-01-01 01:09:11 +0900

    feat: add ads

---
+++ src/components/Ads/rakuten/ads/DpendencyInjectionPrinciplesPracticesAndPatterns.tsx
@@ -0,0 +1,81 @@
+export const DependecyInjectionPriciplesPractilcesAndPatterns = () => {
+  return (
+    <>
+      <table
+        cellPadding={0}
+        cellSpacing="0"
+        border={0}
+        style={{ border: "1px, solid #ccc", width: "300px" }}
+      >
+        <tbody>
+          <tr style={{ borderStyle: "none;" }}>
+            <td
+              style={{
+                verticalAlign: "top",
+                borderStyle: "none",
+                padding: "10px",
+                width: "108px",
+              }}
+            >
+              <a
+                href="https://rpx.a8.net/svt/ejp?a8mat=3ZM4V6+6HDXWY+2HOM+BWGDT&rakuten=y&a8ejpredirect=https%3A%2F%2Fhb.afl.rakuten.co.jp%2Fhgc%2Fg00q0724.2bo11c45.g00q0724.2bo12179%2Fa24121734968_3ZM4V6_6HDXWY_2HOM_BWGDT%3Fpc%3Dhttps%253A%252F%252Fitem.rakuten.co.jp%252Fbook%252F17845616%252F%26amp%3Bm%3Dhttp%253A%252F%252Fm.rakuten.co.jp%252Fbook%252Fi%252F21242123%252F%26amp%3Brafcid%3Dwsc_i_is_33f72da33714639c415e592c9633ecd7"
+                rel="nofollow"
+              >
+                <img
+                  alt=""
+                  src="https://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/3062/9784839983062_1_5.jpg?_ex=128x128"
+                  style={{
+                    padding: 0,
+                    margin: 0,
+                  }}
+                />
+              </a>
+            </td>
+            <td
+              style={{
+                fontSize: "12px",
+                verticalAlign: "middle",
+                borderStyle: "none",
+                padding: "10px",
+              }}
+            >
+              <p style={{ padding: "0", margin: "0" }}>
+                <a
+                  href="https://rpx.a8.net/svt/ejp?a8mat=3ZM4V6+6HDXWY+2HOM+BWGDT&rakuten=y&a8ejpredirect=https%3A%2F%2Fhb.afl.rakuten.co.jp%2Fhgc%2Fg00q0724.2bo11c45.g00q0724.2bo12179%2Fa24121734968_3ZM4V6_6HDXWY_2HOM_BWGDT%3Fpc%3Dhttps%253A%252F%252Fitem.rakuten.co.jp%252Fbook%252F17845616%252F%26amp%3Bm%3Dhttp%253A%252F%252Fm.rakuten.co.jp%252Fbook%252Fi%252F21242123%252F%26amp%3Brafcid%3Dwsc_i_is_33f72da33714639c415e592c9633ecd7"
+                  rel="nofollow"
+                >
+                  なぜ依存を注入するのか　DIの原理・原則とパターン （Compass
+                  Booksシリーズ） [ Steven van Deursen ]
+                </a>
+              </p>
+              <p style={{ color: "#666", marginTop: "5px", lineHeight: "1.5" }}>
+                価格:
+                <span
+                  style={{
+                    fontSize: "14px",
+                    color: "#C00",
+                    fontWeight: "bold",
+                  }}
+                >
+                  4796円
+                </span>
+                <br />
+                <span style={{ fontSize: "10px", fontWeight: "normal" }}>
+                  (2025/1/1 00:54時点)
+                </span>
+                <br />
+                <span style={{ fontWeight: "bold" }}>感想(0件)</span>
+              </p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <img
+        width="1"
+        height="1"
+        src="https://www19.a8.net/0.gif?a8mat=3ZM4V6+6HDXWY+2HOM+BWGDT"
+        alt=""
+      />
+    </>
+  );
+};
