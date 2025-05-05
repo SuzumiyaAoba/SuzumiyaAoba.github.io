5f97cdc7d086318d055bd6b68ba2bb531d25e021
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2025-05-05 18:46:22 +0900

    refactor: update BlogPage component to improve post sorting and rendering logic

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,43 +1,58 @@
 import { Tag } from "@/components/Tag";
 import { Pages } from "@/libs/contents/blog";
 import { getFrontmatters } from "@/libs/contents/markdown";
-// import { getFrontmatters } from "@/libs/contents/blog";
 import { compareDesc, format } from "date-fns";
+import Link from "next/link";
 
-export default async function Home() {
+type PostCardProps = {
+  slug: string;
+  frontmatter: {
+    title: string;
+    date: Date;
+    tags: string[];
+  };
+};
+
+const PostCard = ({ slug, frontmatter }: PostCardProps) => {
+  return (
+    <div>
+      <div className="flex gap-x-1 items-center font-thin">
+        <div className="i-mdi-calendar" />
+        <div>{format(frontmatter.date, "yyyy/MM/dd")}</div>
+      </div>
+      <Link href={`/blog/${slug}/`} className="hover:underline">
+        {frontmatter.title}
+      </Link>
+      <div className="flex flex-wrap mt-2 gap-2 text-xs">
+        {frontmatter.tags.map((tag) => (
+          <Tag key={tag} label={tag} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default async function BlogPage() {
   const posts = await getFrontmatters({
     paths: ["blog"],
     parser: { frontmatter: Pages["blog"].frontmatter },
   });
 
+  const sortedPosts = posts.sort((a, b) =>
+    compareDesc(a.frontmatter.date, b.frontmatter.date)
+  );
+
   return (
     <main className="flex flex-col w-full max-w-4xl mx-auto px-4 pb-16">
       <h1 className="my-8 text-3xl">Blog</h1>
       <div className="flex flex-col gap-6 mb-8">
-        {posts
-          .sort((a, b) => compareDesc(a.frontmatter.date, b.frontmatter.date))
-          .map((blog) => {
-            if (!blog) {
-              return <></>;
-            }
-
-            const { path: slug, frontmatter: frontmatter } = blog;
-
-            return (
-              <div key={slug}>
-                <div className="flex gap-x-1 items-center font-thin">
-                  <div className="i-mdi-calendar" />
-                  <div>{format(frontmatter.date, "yyyy/MM/dd")}</div>
-                </div>
-                <a href={`/blog/${slug}/`} className="hover:underline">
-                  {frontmatter.title}
-                </a>
-                <div className="flex flex-wrap mt-2 gap-2 text-xs">
-                  {frontmatter.tags.map((tag) => <Tag key={tag} label={tag} />)}
-                </div>
-              </div>
-            );
-          })}
+        {sortedPosts.map((post) => (
+          <PostCard
+            key={post.path}
+            slug={post.path}
+            frontmatter={post.frontmatter}
+          />
+        ))}
       </div>
     </main>
   );
