8f3458fb7e25b31946f5d22ba229807f6f78257f
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2025-05-05 18:49:48 +0900

    fix: update iframe attributes in ContactPage component for improved styling

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,9 +5,7 @@ export default async function Contact() {
         src="https://docs.google.com/forms/d/e/1FAIpQLSeXOZA4Mriinisf4yXq8Y9XxfiSNvWjF_qhg5qFYY8af85bfQ/viewform?embedded=true"
         width="640"
         height="1000"
-        frameBorder="0"
-        marginHeight={0}
-        marginWidth={0}
+        className="border-0 m-0"
       >
         読み込んでいます…
       </iframe>
