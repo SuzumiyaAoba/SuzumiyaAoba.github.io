a9b7cf4698a86b42dfa347913538c59f82050cb4
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2025-05-21 22:33:18 +0900

    feat: tooltip dark-theme

--- src/components/CodeHike/tooltip.tsx
+++ src/components/CodeHike/tooltip.tsx
@@ -1,4 +1,3 @@
-
 import { AnnotationHandler } from "codehike/code"
 import {
   TooltipProvider,
@@ -6,18 +5,24 @@ import {
   TooltipTrigger,
   TooltipContent,
 } from "@/components/ui/tooltip";
+import { useTheme } from "next-themes";
 
 export const tooltip: AnnotationHandler = {
   name: "tooltip",
   Inline: ({ children, annotation }) => {
-    const { query, data } = annotation
+    const { query, data } = annotation;
+    const { resolvedTheme } = useTheme();
+    
     return (
       <TooltipProvider>
         <Tooltip>
           <TooltipTrigger className="underline decoration-dashed">
             {children}
           </TooltipTrigger>
-          <TooltipContent align="start">
+          <TooltipContent 
+            align="start"
+            className={`${resolvedTheme === "dark" ? "border-gray-700 bg-gray-800 text-gray-200" : ""}`}
+          >
             {data?.children || query}
           </TooltipContent>
         </Tooltip>
