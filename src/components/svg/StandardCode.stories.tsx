f2c5123eaa3f83fddbe2702223bc4051e9f8178f
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2025-06-22 19:54:02 +0900

    fix(storybook): import StandardCode default export in story

--- src/components/svg/StandardCode.stories.tsx
+++ src/components/svg/StandardCode.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { StandardCode } from "./StandardCode";
+import StandardCode from "./StandardCode";
 
 export default {
   title: "Components/Svg/StandardCode",
